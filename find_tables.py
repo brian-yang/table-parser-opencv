14795e13cd0b6117039a907661ffe01a8b320c54
Brian Yang <brian.yang328@gmail.com>
2017-12-19 16:19:53 -0600

    Get list of table joints

--- find_tables.py
+++ find_tables.py
@@ -9,12 +9,11 @@ image = cv.imread(folder + "table.jpg")
 # Resize image
 # size = (800, 900) # rows and columns in image
 # resized = cv.resize(image, size)
-resized = image.copy()
 
 # Convert resized RGB image to grayscale
 NUM_CHANNELS = 3
 if len(image.shape) == NUM_CHANNELS:
-    grayscale = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
+    grayscale = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 # =====================================================
 # Adaptive thresholding
@@ -60,19 +59,21 @@ vertical_structure = cv.getStructuringElement(cv.MORPH_RECT, (1, vertical_size))
 utils.isolate_lines(vertical, vertical_structure)
 
 # =====================================================
-# Create mask with both horizontal and vertical lines
+# Create an image mask with just the horizontal 
+# and vertical lines in the image
 mask = horizontal + vertical
 
-# Find contours
+# Find all contours in the mask
 (_, contours, _) = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-# Find joints
-joints = cv.bitwise_and(horizontal, vertical)
+# Find intersections between the lines
+corners = cv.bitwise_and(horizontal, vertical)
 
 # =====================================================
 MIN_TABLE_AREA = 100 # min table area to be considered a table
 EPSILON = 3 # epsilon value for contour approximation
-regions_of_interest = [] # list of possible tables
+tables = [] # list of tables
+table_joints = [] # list of locations of table joints
 for i in range(len(contours)):
     area = cv.contourArea(contours[i])
 
@@ -87,39 +88,46 @@ for i in range(len(contours)):
 
     # Find the number of joints in each region of interest (ROI)
     # format: image_mat[rect.x: rect.x + rect.w, rect.y: rect.y + rect.h]
-    joints_roi = joints[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
-    (_, joints_contours, _) = cv.findContours(joints_roi, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+    corner_roi = corners[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
+    (_, possible_table_joints, _) = cv.findContours(corner_roi, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
 
-    # Determines the number of corners in the image
-    # If less than 5 corners, then the image
+    # Determines the number of table joints in the image
+    # If less than 5 table joints, then the image
     # is likely not a table
-    if len(joints_contours) < 5:
+    if len(possible_table_joints) < 5:
         continue
 
-    regions_of_interest.append( resized[rect[1]:rect[1] + rect[3] , rect[0]:rect[0] + rect[2]] )
-    
+    # Store the table images in a list
+    tables.append( image[rect[1]:rect[1] + rect[3] , rect[0]:rect[0] + rect[2]] )
+
+    # Get an ndarray of the coordinates of the table joints
+    joint_coordinates = []
+    for i in range(len(possible_table_joints)):
+        joint_coordinates.append(possible_table_joints[i][0])
+    joint_coordinates = np.asarray(joint_coordinates)
+
 #    resized_copy = resized.copy()
 #    cv.rectangle(resized_copy, (rect[0], rect[1]) , (rect[0] + rect[2], rect[1] + rect[3]) , (0, 255, 0), 1, 8, 0)
 
 # =====================================================
-# Extract tables
-for i in range(len(regions_of_interest)):
-    edges = cv.Canny(regions_of_interest[i], 200, 300, apertureSize = 3) # edge detection - edges include lines, curves, etc.
-    lines = cv.HoughLinesP(edges, 1, np.pi/180, 120, minLineLength = 70, maxLineGap = 20) # line detection
-
-    table_borders = []
-    # print lines
-    for j in range(len(lines)):
-        table_borders.append(lines[j][0].tolist())
-
-    (horizontal_borders, vertical_borders) = utils.sortLines(table_borders)
-
-    for border in horizontal_borders:
-        cv.line(regions_of_interest[i], (border[0], border[1]), (border[2], border[3]), (0, 0, 255), 2)
-
-    for border in vertical_borders:
-        cv.line(regions_of_interest[i], (border[0], border[1]), (border[2], border[3]), (0, 0, 255), 2)
-
-    cv.imshow("roi", regions_of_interest[i])
-    cv.waitKey(0)
-    print()
+# Identify table borders on tables
+#for i in range(len(regions_of_interest)):
+#    edges = cv.Canny(regions_of_interest[i], 200, 300, apertureSize = 3) # edge detection - edges include lines, curves, etc.
+#    lines = cv.HoughLinesP(edges, 1, np.pi/180, 120, minLineLength = 70, maxLineGap = 20) # line detection
+#
+#    table_borders = []
+#    # print lines
+#    for j in range(len(lines)):
+#        table_borders.append(lines[j][0].tolist())
+#
+#    (horizontal_borders, vertical_borders) = utils.sortLines(table_borders)
+#
+#    for border in horizontal_borders:
+#        cv.line(regions_of_interest[i], (border[0], border[1]), (border[2], border[3]), (0, 0, 255), 2)
+#
+#    for border in vertical_borders:
+#        cv.line(regions_of_interest[i], (border[0], border[1]), (border[2], border[3]), (0, 0, 255), 2)
+#
+#    cv.imshow("roi", regions_of_interest[i])
+#    cv.waitKey(0)
+#    print()
